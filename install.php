190489144e23d21047d19a28bee867e71c5b18a7
pradeepkumar03 <kumarp17648330@gmail.com>
2019-02-02 14:53:40 +0530

    statement commit

--- install.php
+++ install.php
@@ -3,4 +3,9 @@ $color="red";
 $text=pradeep;
 echo "my fav color is $color\n";
 echo "i love $text";
-
+echo "\n";
+echo strlen("hello world");
+echo "\n";
+echo str_word_count("hello world \n");
+define("greeting","welcome to india");
+echo greeting;
