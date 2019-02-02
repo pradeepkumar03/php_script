a52935055bd07b219a8e9e5e2feff37772568a3c
pradeepkumar03 <kumarp17648330@gmail.com>
2019-02-02 14:55:16 +0530

    statement commit

---
+++ statement.php
@@ -0,0 +1,20 @@
+<?php
+$x=20;
+if($x=20)
+{
+    echo "it is same";
+}
+else{
+    echo "it is not same";
+}
+echo "\n";
+$fav="red";
+switch($fav)
+{
+    case "red":
+    echo "my fav color is red";
+    break;
+    default:
+    echo "this is default";
+}
+?>
