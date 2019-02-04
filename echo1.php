d3dcaab957aa849f0aba1530afe6a2c76e4661fb
pradeepkumar03 <kumarp17648330@gmail.com>
2019-02-04 12:22:37 +0530

    echo1.php

---
+++ echo1.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="echo.php" method="post">
+NAME:<input name="text" type="name"><br>
+E-MAIL:<input name="text" type="e-mail"><br>
+<input name="submit">
+</body>
+<html>
